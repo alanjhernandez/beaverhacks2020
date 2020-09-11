1c74bbfe2ffef0b41b34d5b4aafc1f144d5461e9
iamkevin <kevinbower123@gmail.com>
2020-09-11 18:44:16 -0400

    Added forms.py for user input (login and registration) as well as two new pages, register and login

--- Weigh_to_Go/weigh_to_go.py
+++ Weigh_to_Go/weigh_to_go.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'feabff93bfc17d29f4a3f4196c3473c8'
 
 @app.route('/')
 @app.route('/home')
@@ -11,6 +14,16 @@ def home():
 def about():
     return render_template('about.html', title = 'About')
 #^This routes the user to the about page when they add /about to the url
+
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
 
 if __name__== '__main__':
 	app.run(debug=True)
